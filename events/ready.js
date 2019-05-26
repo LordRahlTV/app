c8c1c0baa522ea868b57dd6a09deaad54b7c669b
Glitch (hello-express) <none>
2019-05-26 19:39:13 +0000

    📖⛰ Checkpoint ./komutlar/atatürk.js:4653156/770 ./package.json:4653156/1424 ./events/ready.js:4653156/1150

---
+++ events/ready.js
@@ -0,0 +1,32 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Tüm komutlar çalıştırılıyor!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı! BOT aktif!`);
+  client.user.setStatus("online");
+   var oyun = [
+        "+yardım | 7/24 aktif!",
+        "Müzik komutları | Çal çal oyna",
+        "Yapımcı: Metehan Salman",
+        "DC: ßǤ | MetehanSalmannn#5750",
+        "HER ZAMAN HER YERDE DAHA İLERİYE",
+        "+davet yaz ve bize destek ol!",
+        "+sunucutanıt yaz ve sunucunu tanıt!",
+        "+kurulum yaz ve bot için gerekli kanalları kur!",
+        `${client.users.size} kullanıcıya hizmet veriyor.`,
+        `${client.guilds.size} sunucuya hizmet veriyor.`
+
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setGame(oyun[random], "https://www.twitch.tv/emirhansaracyt");
+        }, 2 * 2500);
+}
