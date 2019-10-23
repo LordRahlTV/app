a2592e668cd72eff6539102401c6fa09bde3b984
Glitch (short-guava) <none>
2019-10-23 22:24:15 +0000

    🚘💽 Checkpoint ./komutlar/kilit.js:7956969/2429

--- komutlar/kilit.js
+++ komutlar/kilit.js
@@ -1,35 +1,47 @@
-const ms = require('ms');
-const ayarlar = require('../ayarlar.json');
+const ms = require("ms");
+const ayarlar = require("../ayarlar.json");
 
-var prefix = ayarlar.prefix
+var prefix = ayarlar.prefix;
 
 exports.run = (client, message, args) => {
   if (!message.guild) {
-  const ozelmesajuyari = new Discord.RichEmbed()
-  .setColor(0xFF0000)
-  .setTimestamp()
-  .setAuthor(message.author.username, message.author.avatarURL)
-  .addField(':warning: Uyarı :warning:', '`kilit` adlı komutu özel mesajlarda kullanamazsın.')
-  return message.author.sendEmbed(ozelmesajuyari); }
+    const ozelmesajuyari = new Discord.RichEmbed()
+      .setColor(0xff0000)
+      .setTimestamp()
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .addField(
+        ":warning: Uyarı :warning:",
+        "`kilit` adlı komutu özel mesajlarda kullanamazsın."
+      );
+    return message.author.sendEmbed(ozelmesajuyari);
+  }
   if (!client.lockit) client.lockit = [];
-  let time = args.join(' ');
-  let validUnlocks = ['release', 'unlock'];
-  if (!time) return message.reply('Doğru kullanım: ' + prefix + 'kilit <süre örneğin: 2 min>');
+  let time = args.join(" ");
+  let validUnlocks = ["release", "unlock"];
+  if (!time)
+    return message.reply(
+      "Doğru kullanım: " + prefix + "kilit <süre örneğin: 2 min>"
+    );
 
   if (validUnlocks.includes(time)) {
-    message.channel.overwritePermissions(message.guild.id, {
-      SEND_MESSAGES: null
-    }).then(() => {
-      message.channel.send('Kanal kilidi açıldı.');
-      clearTimeout(client.lockit[message.channel.id]);
-      delete client.lockit[message.channel.id];
-    }).catch(error => {
-      console.log(error);
-    });
+    message.channel
+      .overwritePermissions(message.guild.id, {
+        SEND_MESSAGES: null
+      })
+      .then(() => {
+        message.channel.send("Kanal kilidi açıldı.");
+        clearTimeout(client.lockit[message.channel.id]);
+        delete client.lockit[message.channel.id];
+      })
+      .catch(error => {
+        console.log(error);
+      });
   } else {
-    message.channel.overwritePermissions(message.guild.id, {
-      SEND_MESSAGES: false
-    }).then(() => {
+    message.channel
+      .overwritePermissions(message.guild.id, {
+        SEND_MESSAGES: false
+      })
+      .then(() => {
       message.channel.send(`Kanal kilitlendi. ${ms(ms(time), { long:true })}`).then(() => {
 
         client.lockit[message.channel.id] = setTimeout(() => {
