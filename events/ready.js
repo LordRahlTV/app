365bfa14faa0e0e7230a829f7cd73b95904d7929
Glitch (lord-ralh) <none>
2019-10-26 01:16:04 +0000

    Rewound to commit 11c1a5f71fb913a525ddd79c03a9feb9704920e8 + 0 edits

--- events/ready.js
+++ events/ready.js
@@ -6,21 +6,20 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Tüm komutlar çalıştırılıyor!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı! BOT aktif!`);
   client.user.setStatus("online");
    var oyun = [
-        "Created By LordRahl",
-        "7/24 Aktif BOT!",  
-        "!davet + Botumuzu ekleyin"
-  
+     "Yazı1",
+     "Yazı2",
+     "Yazı3"
+
     ];
 
     setInterval(function() {
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setActivity(oyun[random], "https://www.twitch.tv/anloxbot");
-        }, 2500);
+       
+        }, 2 * 2500);
 }
