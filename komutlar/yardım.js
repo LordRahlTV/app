c6a7cc9a3536313ea3285215669dca43c7518fca
Glitch (lord-ralh) <none>
2019-10-26 02:02:40 +0000

    🌔😜 Checkpoint ./komutlar/yardım.js:6592920/13265

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,29 +1,29 @@
-
+const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 
 var prefix = ayarlar.prefix;
 
 exports.run = (client, message, params) => {
-  const embedyardim = new Discord.RichEmbed()
-  .setTitle("Komutlar")
-  .setDescription('')
-  .setColor(0x00ffff)
-  .addField("**Eğlence ve Kullanıcı Komutları:**", `!banned = Dene ve Gör! \!avatarım = Avatarınınızı Gösterir. \!herkesebendençay = Herkese Çay Alırsınız. \!koş = Koşarsınız.\!çayiç = Çay İçersiniz. \!çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \!çayaşekerat = Çaya Şeker Atarsınız. \!yumruh-at = Yumruk Atarsınız. \!yaz = Bota İstediğiniz Şeyi Yazdırırsınız. \!sunucuresmi = BOT Sunucunun Resmini Atar. \!sunucubilgi = BOT Sunucu Hakkında Bilgi Verir. \!kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. `)
-  .addField("**Yetkilisi Komutlar**", `!!ban = İstediğiniz Kişiyi Sunucudan Banlar. \!kick  = İstediğiniz Kişiyi Sunucudan Atar. \!unban = İstediğiniz Kişinin Yasağını Açar. \!sustur = İstediğiniz Kişiyi Susturur. \!oylama = Oylama Açar. \!duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
-  .addField("**Ana Komutlar**", "!yardım = BOT Komutlarını Atar. \!bilgi = BOT Kendisi Hakkında Bilgi Verir. \!!ping = BOT Gecikme Süresini Söyler. \!!davet = BOT Davet Linkini Atar. \!istatistik = BOT İstatistiklerini Atar.")
-  .addField("**Yapımcı**", " **Lord Rahl** ")
-  .setFooter('**--------------------------**')
+
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(embedyardim);
+    message.author.sendCode('asciidoc', `= Komut Listesi =\n\n[Komut hakkında bilgi için ${ayarlar.prefix}yardım <komut adı>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Özel mesajlarını kontrol et. :postbox:');
+    message.channel.sendEmbed(ozelmesajkontrol) }
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      message.author.send('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
+      message.author.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
     }
   }
+
 };
 
 exports.conf = {
