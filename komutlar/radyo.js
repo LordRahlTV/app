7b4aed8a6133944c391ed7b8b59c616bd1608a9f
Glitch (lord-ralh) <none>
2019-11-17 00:51:36 +0000

    🚆🐬 Checkpoint ./komutlar/kahkaha.js:6592920/975 ./komutlar/kralol.js:6592920/1009 ./komutlar/kulanıcıara.js:6592920/619 ./komutlar/kur.js:6592920/873 ./komutlar/mcbaşarım.js:6592920/1144 ./komutlar/müzik.js:6592920/908 ./komutlar/nahcek.js:6592920/738 ./komutlar/olay.js:6592920/1021 ./komutlar/radyo.js:6592920/229

---
+++ komutlar/radyo.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["radio"],
+    permLevel: 0
+};
+
+exports.help = {
+    name : "radyo",
+    usage: "radyo",
+    description: "radyo"
+};
