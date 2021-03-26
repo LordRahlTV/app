c73a5702733a3a94eb6aa8868129d3ca4a63c7b0
Glitch (bircanx) <none>
2021-03-26 14:02:15 +0000

    Import of aru-xyz/discord-bos-v12

--- events/ready.js
+++ events/ready.js
@@ -1,26 +1,17 @@
-const chalk = require('chalk');
-const moment = require('moment');
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
+const Discord = require("discord.js");
+const config = require('../config.js');
+module.exports = async client => {
+  client.user.setPresence({ activity: { type: "WATCHING", name: `Piece ❤ Serendia Squad`}, status: 'idle' })
+};
 
-var prefix = ayarlar.prefix;
+// Type kısımları:
+// WATCHING - İZLİYOR
+// PLAYING - OYNUYOR
+// LISTENING - DİNLİYOR
 
-module.exports = client => {
-  console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-   var oyun = [
-        "Created By ＡＩＳ. ＥＲＥＮ",
-        "7/24 Aktif BOT!",  
-        "?davet + Botumuzu ekleyin"
-  
-    ];
+// Status kısımları:
+// online - çevrim içi
+// idle - boşta
+// dnd - rahatsız etmeyin
 
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setActivity(oyun[random], "https://www.twitch.tv/anloxbot");
-        }, 2500);
-}
+// name kısmına oynuyorunuzu yazabilirsiniz.
